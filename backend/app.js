fef75a3be4397d7fb20834d3e200b66d8836027b
elliotracinep <racinenmimie1@gmail.com>
2026-04-02 13:12:24 -0400

    sauvegarde

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-function errorHandler(error, req, res, next) {
-  if (res.headersSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "Une erreur inconnue est survenue !" });
-}
-export default errorHandler;
